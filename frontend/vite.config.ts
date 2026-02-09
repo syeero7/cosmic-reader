df661c5b40dddb2bee77f08379a6ec7ac1192dc7
emal <emalhasitha@proton.me>
2026-02-09 06:50:35 +0530

    build: update vite preact-ts to latest version

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import preact from "@preact/preset-vite";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-	plugins: [preact()],
+  plugins: [preact()],
 });
