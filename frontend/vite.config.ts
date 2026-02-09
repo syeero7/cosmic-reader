d4a6ec5ab6464f44c1e6afaeb9dfa6791675da81
emal <emalhasitha@proton.me>
2026-02-09 17:51:06 +0530

    build: configure typeScript and vite path alias

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,14 @@
+import path from "node:path";
 import preact from "@preact/preset-vite";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@wails": path.resolve(__dirname, "./wailsjs"),
+    },
+  },
 });
